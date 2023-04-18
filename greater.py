d957414ad555ae7547cfca0a0750b8f06491d09f
Praveen kumar <130967293+Praveenkumar2024@users.noreply.github.com>
2023-04-18 12:55:00 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,9 @@
+a=int(input("enter the nnumber="))
+b=int(input("enter the nnumber="))
+c=int(input("enter the nnumber="))
+if(a>b AND a>c)
+    print(a ,"number greater');
+elif(b>c and a>c
+     print(b ," nummber is greater")
+else
+    print(c ," number is greater')
